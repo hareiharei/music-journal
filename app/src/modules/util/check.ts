a92c987782e23349844a52cb289ed099b77ac655
hareiharei <harei.whitewings@gmail.com>
2025-04-17 00:19:15 +0900

    `isNullOrEmptyList`を実装

--- app/src/modules/util/check.ts
+++ app/src/modules/util/check.ts
@@ -3,3 +3,9 @@ export const isNullOrEmptyString = (
 ) => {
   return value === null || value === ''
 }
+
+export const isNullOrEmptyList = (
+  value: any[] | null
+) => {
+  return value === null || value.length === 0
+}
