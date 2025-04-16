1f2fb81150cf9c54e2d3ac062281d1f2f4b9ad0e
hareiharei <harei.whitewings@gmail.com>
2025-04-16 15:08:47 +0900

    `Photo`関連の実装を追加

--- app/src/modules/domain/Photo/Photo.ts
+++ app/src/modules/domain/Photo/Photo.ts
@@ -1,22 +1,40 @@
 import { UUIDValueObject } from "../shared/UUIDValueObject";
 
 export class Photo {
-  constructor(
+  private constructor(
     public readonly id: PhotoId,
     public description: PhotoDescription | null,
   ) {}
 
   // TODO: 
+  static of(
+    description: string | null
+  ): Photo {
+    if (description === null) {
+      return new Photo(
+        PhotoId.new(),
+        null,
+      )
+    }
+    
+    return new Photo(
+      PhotoId.new(),
+      PhotoDescription.of(description),
+    )
+  }
 }
 
 export class PhotoId extends UUIDValueObject {}
 
 export class PhotoDescription {
-  constructor(
+  private constructor(
     public readonly value: string
   ) {
     // TODO: 文字列の長さの上限
   }
 
   // TODO: ユーザーから入力された
+  static of(value: string): PhotoDescription {
+    return new PhotoDescription(value)
+  }
 }
