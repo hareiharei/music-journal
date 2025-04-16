271d353a088a82fa81807bc02310f8eabdbbe546
hareiharei <harei.whitewings@gmail.com>
2025-04-16 14:06:13 +0900

    `Venue`に関連する値オブジェクトの実装を追加

--- app/src/modules/domain/Venue/Venue.ts
+++ app/src/modules/domain/Venue/Venue.ts
@@ -1,34 +1,54 @@
 export class Venue {
-  constructor(
+  private constructor(
     public readonly name: VenueName,
     public readonly addressURL: VenueAddressURL | null,
   ) {}
-  
-  // TODO: ユーザーに入力された
+
+  static of(
+    name: string,
+    addressURL: string | null,
+  ): Venue {
+    if (addressURL === null || addressURL === '') {
+      return new Venue(
+        VenueName.of(name),
+        null,
+      )
+    }
+
+    return new Venue(
+      VenueName.of(name),
+      VenueAddressURL.of(addressURL),
+    )
+  }
 
   // TODO: Google Mapから取得された
 }
 
 export class VenueName {
-  constructor(
+  private constructor(
     public readonly value: string,
   ) {
-    // TODO: 空文字を許容しない
+    if (value === '') throw new Error('Venue Name should not be an empty string')
 
     // TODO: 文字列の長さの上限
   }
 
   // TODO: ユーザーに入力された
+  static of(value: string): VenueName {
+    return new VenueName(value)
+  }
 
   // TODO: URLのGoogle Mapの情報から取得された
 }
 
 export class VenueAddressURL {
-  constructor(
+  private constructor(
     public readonly value: string,
   ) {
-    // TODO: 空文字を許容しない
+    if (value === '') throw new Error('Venue Address URL should not be an empty string')
   }
 
-  // TODO: ユーザーに入力された
+  static of(value: string): VenueAddressURL {
+    return new VenueAddressURL(value)
+  }
 }
