51c8c981b5defa0e0b919b74e982bf42659e2549
hareiharei <harei.whitewings@gmail.com>
2025-04-20 03:22:35 +0900

    Artistのテストケース

---
+++ app/test/modules/domain/Artist/Artist.test.ts
@@ -0,0 +1,38 @@
+describe('Artist', () => {
+  describe('of', () => {
+    it('shuold instantiate when name is a non-empty string and spotifyArtistURL is a non-empty string', () => {
+      
+    })
+    it('shuold instantiate when spotifyArtistURL is null', () => {
+
+    })
+    it('shuold instantiate when spotifyArtistURL is an empty string', () => {
+      
+    })
+    it('shuold return error when name is an empty string', () => {
+      
+    })
+  })
+})
+
+describe('ArtistName', () => {
+  describe('of', () => {
+    it('should instantiate', () => {
+
+    })
+    it('shuold return error when value is an empty string', () => {
+      
+    })
+  })
+})
+
+describe('SpotifyArtistURL', () => {
+  describe('of', () => {
+    it('shuold instantiate', () => {
+      
+    })
+    it('shuold return error when value is an empty string', () => {
+
+    })
+  })
+})
