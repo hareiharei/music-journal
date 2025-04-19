11eb9ea8c1d2e2c6ac3b8da7bd3ccd158afd515d
hareiharei <harei.whitewings@gmail.com>
2025-04-20 02:36:36 +0900

    `isNullOrEmptyString`のテストを実装

--- app/test/shared/util/check.test.ts
+++ app/test/shared/util/check.test.ts
@@ -1,8 +1,19 @@
-// test isNullOrEmptyString
-// should return true when value is null
-// should return true when value is an empty string
-// should return false when value is a non-empty string
+import { isNullOrEmptyString } from "@/shared/util/check"
 
+describe('isNullOrEmptyString', () => {
+  it('should return true when value is null', () => {
+    const result = isNullOrEmptyString(null)
+    expect(result).toBe(true)
+  })
+  it('should return true when value is an empty string', () => {
+    const result = isNullOrEmptyString('')
+    expect(result).toBe(true)
+  })
+  it('should return false when value is a non-empty string', () => {
+    const result = isNullOrEmptyString('t')
+    expect(result).toBe(false)
+  })
+})
 // test isEmptyString
 // should return true when value is an empty string
 // shuold return false when value is a non-empty string
