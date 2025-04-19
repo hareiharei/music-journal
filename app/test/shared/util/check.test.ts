aa3d09968aa1c4c09c00d3d727b21f9b55ac4040
hareiharei <harei.whitewings@gmail.com>
2025-04-20 02:45:42 +0900

    `isNullOrEmptyList`のテストを実装

--- app/test/shared/util/check.test.ts
+++ app/test/shared/util/check.test.ts
@@ -1,4 +1,4 @@
-import { isEmptyString, isNullOrEmptyString } from "@/shared/util/check"
+import { isEmptyString, isNullOrEmptyList, isNullOrEmptyString } from "@/shared/util/check"
 
 describe('isNullOrEmptyString', () => {
   it('should return true when value is null', () => {
@@ -26,10 +26,20 @@ describe('isEmptyString', () => {
   })
 })
 
-// test isNullOrEmptyList
-// should return true when value is null
-// should return true when value is an empty list
-// should return false when value is a non-empty list
+describe('isNullOrEmptyList', () => {
+  it('should return true when value is null', () => {
+    const result = isNullOrEmptyList(null)
+    expect(result).toBe(true)
+  })
+  it('should return true when value is an empty list', () => {
+    const result = isNullOrEmptyList([])
+    expect(result).toBe(true)
+  })
+  it('should return false when value is a non-empty list', () => {
+    const result = isNullOrEmptyList([{'test': 1}])
+    expect(result).toBe(false)
+  })
+})
 
 // test isEmptyList
 // should return true when value is an empty list
