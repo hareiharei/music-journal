6e9307a1a69ee57c84ca74b897d1eea89982cb05
hareiharei <harei.whitewings@gmail.com>
2025-04-20 02:18:22 +0900

    check.tsのテストケース

---
+++ app/test/shared/util/check.test.ts
@@ -0,0 +1,17 @@
+// test isNullOrEmptyString
+// should return true when value is null
+// should return true when value is an empty string
+// should return false when value is a non-empty string
+
+// test isEmptyString
+// should return true when value is an empty string
+// shuold return false when value is a non-empty string
+
+// test isNullOrEmptyList
+// should return true when value is null
+// should return true when value is an empty list
+// should return false when value is a non-empty list
+
+// test isEmptyList
+// should return true when value is an empty list
+// should return false when value is a non-empty list
