499b9180ef8940dfe0dbbb3bebdc63cd1e65a648
hareiharei <harei.whitewings@gmail.com>
2025-04-21 02:17:34 +0900

    Songのテストケース

---
+++ app/test/modules/domain/Song/Song.test.ts
@@ -0,0 +1,38 @@
+describe('Song', () => {
+  describe('of', () => {
+    it('should instantiate when title is a non-empty string and spotifySongURL is a non-empty string', () => {
+
+    })
+    it('should instantiate when spotifySongURL is null', () => {
+
+    })
+    it('should instantiate when spotifySongURL is an empty string', () => {
+
+    })
+    it('should return error when title is an empty string', () => {
+      
+    })
+  })
+})
+
+describe('SongTitle', () => {
+  describe('of', () => {
+    it('should instantiate when value is a non-empty string', () => {
+
+    })
+    it('should return error when value is an empty string', () => {
+
+    })
+  })
+})
+
+describe('SpotifySongURL', () => {
+  describe('of', () => {
+    it('should instantiate when value is a non-empty string'), () => {
+      
+    }
+    it('should return error when value is an empty string', () => {
+
+    })
+  })
+})
