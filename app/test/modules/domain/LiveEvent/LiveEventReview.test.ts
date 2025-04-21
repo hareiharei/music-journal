285e3468c59dd223f306e9b1d1ecee6d23e70eb7
hareiharei <harei.whitewings@gmail.com>
2025-04-21 23:16:06 +0900

    LiveEventReviewのテストケース

---
+++ app/test/modules/domain/LiveEvent/LiveEventReview.test.ts
@@ -0,0 +1,25 @@
+describe('LiveEventReview', () => {
+  describe('create', () => {
+    it('should instantiate when value is a non-empty string', () => {
+    })
+    it('should return error when value is an empty string', () => {
+    })
+  })
+  describe('edit', () => {
+    it('should update value when value is a non-empty string', () => {
+    })
+    it('should return error when value is an empty string', () => {
+    })
+  })
+  describe('delete', () => {
+    it('should return DeletedLiveEventReview', () => {
+    })
+  })
+})
+
+describe('DeletedLiveEventReview', () => {
+  describe('of', () => {
+    it('should instantiate', () => {
+    })
+  })
+})
