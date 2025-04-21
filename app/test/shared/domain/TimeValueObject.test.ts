abdd2fa7009ce6e481e27dd596873615f8b8f3bc
hareiharei <harei.whitewings@gmail.com>
2025-04-22 00:27:21 +0900

    TimeValueObjectのテストケース

---
+++ app/test/shared/domain/TimeValueObject.test.ts
@@ -0,0 +1,26 @@
+describe('TimeValueObject', () => {
+  describe('of', () => {
+    it('should instantiate when value is in a valid time format', () => {
+    })
+    it('should return error when value is an empty string', () => {
+    })
+    it('should return error when value is in an invalid time format', () => {
+    })
+  })
+  describe('isBefore', () => {
+    it('should return true when this value is before the other value', () => {
+    })
+    it('should return false when this value is same as the other value', () => {
+    })
+    it('should return false when this value is after the other value', () => {
+    })
+  })
+  describe('isAfter', () => {
+    it('should return true when this value is after the other value', () => {
+    })
+    it('should return false when this value is same as the other value', () => {
+    })
+    it('should return false when this value is before the other value', () => {
+    })
+  })
+})
