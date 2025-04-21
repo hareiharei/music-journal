be5fbdcb43974207e1a9fe69fe1bc1b27d3b34e5
hareiharei <harei.whitewings@gmail.com>
2025-04-21 13:25:19 +0900

    Photoのテストケース

---
+++ app/test/modules/domain/Photo/Photo.test.ts
@@ -0,0 +1,10 @@
+describe('Photo', () => {
+  describe('of', () => {
+    it('should instantiate', () => {
+    })
+  })
+  describe('fromStore', () => {
+    it('should instantiate', () => {
+    })
+  })
+})
