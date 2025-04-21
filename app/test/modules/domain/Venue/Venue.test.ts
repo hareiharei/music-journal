a49e71722d767c8ecd0ec4ef59433cffa4a392a5
hareiharei <harei.whitewings@gmail.com>
2025-04-21 12:51:55 +0900

    Venueのテストケース

---
+++ app/test/modules/domain/Venue/Venue.test.ts
@@ -0,0 +1,30 @@
+describe('Venue', () => {
+  describe('of', () => {
+    it('should instantiate when name is a non-empty string and addressURL is a non-empty string', () => {
+    })
+    it('should instantiate when addressURL is an empty string', () => {
+    })
+    it('should instantiate when addressURL is null', () => {
+    })
+    it('should return error when name is an empty string', () => {
+    })
+  })
+})
+
+describe('VenueName', () => {
+  describe('of', () => {
+    it('should instantiate when value is a non-empty string', () => {
+    })
+    it('should return error when value is an empty string', () => {
+    })
+  })
+})
+
+describe('VenueAddressURL', () => {
+  describe('of', () => {
+    it('should instantiate when value is a non-empty string', () => {
+    })
+    it('should return error when value is an empty string', () => {
+    })
+  })
+})
