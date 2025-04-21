245a67302d573c73de24cf003be8cce6418c115f
hareiharei <harei.whitewings@gmail.com>
2025-04-22 00:18:30 +0900

    UUIDValueObjectのテストケース

---
+++ app/test/shared/domain/UUIDValueObject.test.ts
@@ -0,0 +1,14 @@
+describe('UUIDValueObject', () => {
+  describe('new', () => {
+    it('should return UUIDValueObject with random v4 UUID', () => {
+    })
+  })
+  describe('of', () => {
+    it('should instantiate with v4 UUID', () => {
+    })
+    it('should return error when value is a UUID of a version other than v4', () => {
+    })
+    it('should return error when value is not a UUID', () => {
+    })
+  })
+})
