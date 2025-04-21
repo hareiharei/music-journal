4cf504abb84361330ab9f934331f90422c860df5
hareiharei <harei.whitewings@gmail.com>
2025-04-22 00:12:18 +0900

    LiveEventのテストケース

---
+++ app/test/modules/domain/LiveEvent/LiveEvent.test.ts
@@ -0,0 +1,72 @@
+describe('LiveEvent', () => {
+  describe('create', () => {
+    it('should instantiate when startTime, endTime, detail, venues, and artists are non-empty strings or lists', () => {
+    })
+    it('should return error when startTime, endTime, detail, venues, and artists are empty strings or lists', () => {
+    })
+    it('should return error when startTime, endTime, detail, venues, and artists are null', () => {
+    })
+    it('should return error when endTime is before startTime', () => {
+    })
+  })
+  describe('fromStore', () => {
+    it('should instantiate when startTime, endTime, detail, venues, and artists are non-empty strings or lists', () => {
+    })
+    it('should return error when startTime, endTime, detail, venues, and artists are empty strings or lists', () => {
+    })
+    it('should return error when startTime, endTime, detail, venues, and artists are null', () => {
+    })
+    it('should return error when endTime is before startTime', () => {
+    })
+  })
+  describe('edit', () => {
+    it('should update when startTime, endTime, detail, venues, and artists are non-empty strings or lists', () => {
+    })
+    it('should return error when startTime, endTime, detail, venues, and artists are empty strings or lists', () => {
+    })
+    it('should return error when startTime, endTime, detail, venues, and artists are null', () => {
+    })
+    it('should return error when endTime is before startTime', () => {
+    })
+  })
+  describe('delete', () => {
+    it('should return DeletedLiveEvent', () => {
+    })
+  })
+})
+
+describe('DeletedLiveEvent', () => {
+  describe('of', () => {
+    it('should instantiate', () => {
+    })
+  })
+})
+
+describe('LiveEventTitle', () => {
+  it('should instantiate when value is a non-empty string', () => {
+  })
+  it('should instantiate when value is an empty string', () => {
+  })
+})
+
+describe('LiveEventDate', () => {
+  it('should instantiate', () => {
+  })
+})
+
+describe('LiveEventStartTime', () => {
+  it('should instantiate', () => {
+  })
+})
+
+describe('LiveEventEndTime', () => {
+  it('should instantiate', () => {
+  })
+})
+
+describe('LiveEventDetail', () => {
+  it('should instantiate when value is a non-empty string', () => {
+  })
+  it('should instantiate when value is an empty string', () => {
+  })
+})
