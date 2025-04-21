d6eb66e8f41e00eea05ff21c08617944095aa196
hareiharei <harei.whitewings@gmail.com>
2025-04-21 13:46:39 +0900

    LiveEventPhotoのテストケース

---
+++ app/test/modules/domain/LiveEvent/LiveEventPhoto.test.ts
@@ -0,0 +1,37 @@
+describe('LiveEventPhoto', () => {
+  describe('of', () => {
+    it('should instantiate when description is a non-empty string', () => {
+    })
+    it('should instantiate when description is null', () => {
+    })
+    it('should instantiate when description is an empty string', () => {
+    })
+  })
+  describe('updateDescription', () => {
+    it('should update description when it is a non-empty string', () => {
+    })
+    it('should update description when it is null', () => {
+    })
+    it('should update description when it is an empty string')
+  })
+  describe('delete', () => {
+    it('should return DeletedLiveEventPhoto', () => {
+    })
+  })
+})
+
+describe('DeletedLiveEventPhoto', () => {
+  describe('of', () => {
+    it('should instantiate', () => {
+    })
+  })
+})
+
+describe('PhotoDescription', () => {
+  describe('of', () => {
+    it('should instantiate when value is a non-empty string', () => {
+    })
+    it('should return error when value is an empty string', () => {
+    })
+  })
+})
